df8bb26cf40181bfdaa4c48423188863d597fc1c
T-Berger <thomas.be98@googlemail.com>
2018-04-05 14:48:15 +0200

    Stand Besprechung von 4.4.18

--- src/main/java/JsonToXmlMealyMachine.java
+++ src/main/java/JsonToXmlMealyMachine.java
@@ -8,9 +8,14 @@ import java.nio.file.StandardWatchEventKinds;
 
 import java.util.List;
 
-import javafx.scene.shape.Path;
-import org.json.JSONException;
-import static java.nio.file.StandardWatchEventKinds.*;
+// import javafx.scene.shape.Path;
+import org.json.JSONException; //needs the json-20090211.jar to run saved at ./json-20090211.jar saved
+// import static java.nio.file.StandardWatchEventKinds.*;
+
+// TODO: 05.04.18  1 more State/ Symbol for the Conversion
+// TODO: 05.04.18  Polling and thread communication
+// TODO: Tabelle für Ausgabefunktion?
+// Wahrscheinlich keine Änderung mehr bis zur nächsten Woche da Teilnahme an Exkursion
 
 
 public class JsonToXmlMealyMachine {
@@ -33,6 +38,7 @@ public class JsonToXmlMealyMachine {
 
     enum Symbol{
         transInit (0){
+            // reads the json File in & saves it the Variable String Xml for his constructor  Symbol.transinit.xml
             @Override
             String output(String input) throws IOException, JSONException {
                 System.out.println("Verarbeite Eingabe");
@@ -56,6 +62,7 @@ public class JsonToXmlMealyMachine {
             }
         },
         transFinal (1){
+            // Output there converted Json file as Xml
             @Override
             String output(String input) throws IOException {
                 System.out.println("XML:");
@@ -74,6 +81,7 @@ public class JsonToXmlMealyMachine {
             }
         },
         transError (2){
+            // Simply an error state
             @Override
             String output(String input) {
                 setNextSymbol(Symbol.transError);
@@ -84,73 +92,68 @@ public class JsonToXmlMealyMachine {
         private final int currentSymbol;
         private Symbol nextSymbol;
         private Symbol previousSymbol;
+        // default enum constructor
         Symbol (int currentSymbol){
             this.currentSymbol = currentSymbol;
         }
-
+        // getter for the current Symbol step/ process
         public int getCurrentSymbol(){ return currentSymbol;}
 
         static public final Integer symbolLength= Symbol.values().length;
 
         abstract String output(String input) throws IOException, JSONException;
 
+        // Readed/edited JsonToXml String from the constructor is saved  here
+        private String xml;
+
+        // Get & Setters to for working and modifying the enum Symobl
         public void setNextSymbol(Symbol nextSymbol) {
             this.nextSymbol = nextSymbol;
         }
         public Symbol getNextSymbol() {
             return nextSymbol;
         }
-        private String xml;
         public void setXml(String xml) {
             this.xml = xml;
         }
-
         public Symbol getPreviousSymbol() {
             return previousSymbol;
         }
         public void setPreviousSymbol(Symbol previousSymbol) {
             this.previousSymbol = previousSymbol;
         }
-
         public String getXml() {
             return xml;
         }
     }
+    // the Transition Table for the Mealy Machine; CurrentStates + CurrentTransiton -transitionTable-> new State
     static State transitonTable [][] = {
             //       transInit       transFinal       transError
             {
-                    State.Final,  State.Final,    State.Error  //  Initial
+                    State.Final,  State.Final,    State.Error   //  Initial
             }, {
-            State.Initial,    State.Error,  State.Error // Final
-    }, {
-            State.Error,    State.Error,    State.Error // Error
+                    State.Initial,    State.Error,  State.Error // Final
+            }, {
+                    State.Error,    State.Error,    State.Error // Error
     }
 
     };
+    /**@param symbolValue Index from the current EnumConstructorSymbol
+     * @param stateValue Index from the current EnumConstructorState
+     * @return Return a new State with the help of the Transition Table
+    */
     static public State transitionMachine (int symbolValue, int stateValue){
         System.out.println(transitonTable);
         return transitonTable[symbolValue][stateValue];
     }
-    //    public State transitionMachine (int symbolValue, int stateValue){
-//        return transitonTable[symbolValue][stateValue];
-//    }
-//    private class TranisitonMachine < T extends State, V extends Symbol> {
-//        TranisitonMachine ( T symbol, V state, T[][] transTable, T init){
-//            if(transTable.length != T.stateLength || transTable[0].length != V.symbolLength) {
-//                throw ArrayIndexOutOfBoundsException;
-//            }
-//           transitonTable[0][1]
-//
-//        }
-//
-//    }
-    public String xml;
+
     public static String convertJsonToXML(String json, String root) throws JSONException {
         org.json.JSONObject jsonFileObject = new org.json.JSONObject(json);
         String xml = "<?xml version=\"1.0\" encoding=\"ISO-8859-15\"?>\n<"+root+">"
                 + org.json.XML.toString(jsonFileObject) + "</"+root+">";
         return xml;
     }
+
     public static String readFile(String filepath) throws FileNotFoundException, IOException {
         StringBuilder sb = new StringBuilder();
         InputStream in = new FileInputStream(InputPath);
@@ -169,6 +172,10 @@ public class JsonToXmlMealyMachine {
 
         return sb.toString();
     }
+    /**
+     * @param filepath for the output File
+     * @param output --> the Xml String, which was converted from the Json String
+     */
     public static void writeXmlFile(String filepath, String output) throws FileNotFoundException, IOException {
         FileWriter ofstream = new FileWriter(filepath);
         try (BufferedWriter out = new BufferedWriter(ofstream)) {
@@ -179,7 +186,8 @@ public class JsonToXmlMealyMachine {
     private static String InputPath = "input.json";
     private static String OutputPath = "./output.xml";
 
-    public static void main(String[] args) throws IOException, JSONException {
+    public static void main (String[] args) throws IOException, JSONException {
+
         State current = State.Initial;
         Symbol symbol = Symbol.transInit;
 
@@ -188,70 +196,47 @@ public class JsonToXmlMealyMachine {
 
         OutputStream output = System.out;
 
+        // @TODO File Watcher APi
+//
+//        java.nio.file.Path dir = Paths.get("./json");
+//        try {
+//            WatchService watcher = dir.getFileSystem().newWatchService();
+//            dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,
+//                    StandardWatchEventKinds.ENTRY_MODIFY);
+//
+//            WatchKey watckKey = watcher.take();
+//
+//            List<WatchEvent<?>> events = watckKey.pollEvents();
+//            for (WatchEvent event : events) {
+//                if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+//                    System.out.println("Created: " + event.context().toString());
+//
+//                }
+//                if (event.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
+//                    System.out.println("Modify: " + event.context().toString());
+//                }
+//            }
+//
+//        } catch (Exception e) {
+//            System.out.println("Error: " + e.toString());
+//        }
+        // @TODO
 
-
-
-
-        java.nio.file.Path dir = Paths.get("./json");
-        try {
-            WatchService watcher = dir.getFileSystem().newWatchService();
-            dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,
-                    StandardWatchEventKinds.ENTRY_MODIFY);
-
-            WatchKey watckKey = watcher.take();
-
-            List<WatchEvent<?>> events = watckKey.pollEvents();
-            for (WatchEvent event : events) {
-                if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
-                    System.out.println("Created: " + event.context().toString());
-
-                }
-                if (event.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
-                    System.out.println("Modify: " + event.context().toString());
-                }
-            }
-
-        } catch (Exception e) {
-            System.out.println("Error: " + e.toString());
-        }
-
-
-
-
-
-
-        // System.out.println(symbol.getCurrentSymbol()+ " kkkk " + current.currentState);
-        // current = transitionMachine(0,2);
-        //System.out.println(transitionMachine(symbol.getCurrentSymbol(),current.getCurrentState()));
-//                current = transitionMachine(symbol.getCurrentSymbol(),current.getCurrentState());
-//                System.out.println(current);
-//                symbol.output(symbol.currentSymbol);
-//                System.out.println(symbol.getNextSymbol());
-//                symbol = symbol.getNextSymbol();
-//                System.out.println(symbol.getCurrentSymbol());
+        // Mealy Machine process
         while (current != State.Error){
-//            System.out.println("Get in there");
-////            System.out.println(symbol.getCurrentSymbol()+ " kkkk " + current.currentState);
-//            current = transitionMachine(symbol.getCurrentSymbol(),current.getCurrentState());
-//            symbol.currentSymbol.output
-//            symbol = symbol.getNextSymbol();
+            // Get the new State
             current = transitionMachine(symbol.getCurrentSymbol(),current.getCurrentState());
             System.out.println(current);
-            symbol.output("LEER");
+            // Starts the processing Step from the current Transition
+            symbol.output("ROOT");
             System.out.println(symbol.getXml());
             System.out.println(symbol.getNextSymbol());
+            // Save the old Symbol in previous śymbol and set the current symbol to the next
             Symbol tmp = symbol;
             symbol = symbol.getNextSymbol();
             symbol.previousSymbol = tmp;
             System.out.println(symbol.getCurrentSymbol());
-//            if(current == State.Final){
-//                System.out.println("DEBUG");
-//              //  symbol.output("LEER");
-//                break;
-//            }
         }
     }
-    // https://github.com/beckchr/staxon/wiki/Converting-JSON-to-XML
-
 
 }
